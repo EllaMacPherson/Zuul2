8679dcf6323b7b7ff6768513417f2163f512285f
Ella MacPherson <ella.mm.macpherson@gmail.com>
2025-11-23 18:28:41 -0800

    everything is falling apart again. idk how to item

---
+++ room.h
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <map> //figure out how to use lol.
+
+
+using namespace std;
+
+
+class Room{
+ public:
+  //func to set description
+  Room(const char* inDescription);
+  //set exits with map?
+  void setExit(const char*, Room*);
+  //print when they enter such room
+  void print();
+  //map variable for each room, first var is direction of exit , second var is WHERE that exit goes too
+  map<const char*, Room*> exitMap;
+  
+  struct Item{
+    const char* itemDescription;
+  };
+
+  vector<Item*> itemsInRoom;
+  void addItem(const char*); 
+  vector<Item*> getItems();
+  
+
+ protected:
+  //location
+  char* description;
+  //vector of items in room.
+  vector<Item*> roomItems;
+};
